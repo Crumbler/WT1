af893194de97fdcc06822c402a25d8fff59242a5
Crumbler <25861574+Crumbler@users.noreply.github.com>
2021-10-10 20:14:31 +0300

    First 2 tasks

--- src/webTech/WebTech.java
+++ src/webTech/WebTech.java
@@ -2,9 +2,34 @@ package webTech;
 
 public class WebTech {
 
+	private static float task1(float x, float y)
+	{
+		float sub1 = (float)(1.0f + Math.sin(x + y) * Math.sin(x + y));
+		float sub2 = 2.0f + Math.abs(x - 2.0f * x / (1.0f + x * x * y * y));
+		
+		return x + sub1 / sub2;
+	}
+	
+	private static boolean task2(int x, int y)
+	{
+		int xAbs = Math.abs(x);
+		
+		if (y <= 0 && y >= -3 && xAbs <= 6)
+		{
+			return true;
+		}
+		
+		if (y >= 0 && y <= 5 && xAbs <= 4)
+		{
+			return true;
+		}
+			
+		return false;
+	}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello, World");
+		
+		System.out.println(task2(6, -4));
 		
 	}
 
